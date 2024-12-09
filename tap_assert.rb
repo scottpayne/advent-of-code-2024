e420858a438ea8bd3096aa350ace8f4477612978
Scotty P <scottyp@scottpayne.id.au>
2024-12-10 06:22:54 +1100

    Proper TAP format

--- tap_assert.rb
+++ tap_assert.rb
@@ -3,6 +3,6 @@
 # I don't want to introduce Rspec of minitest to do adhoc testing of my puzzle solutions,
 # this is a simple inline version for adhoc testing.
 def assert(expected, actual, message = "Expect: #{expected}, Actual: #{actual}")
-  result = (expected == actual) ? "OK" : "FAIL"
-  puts "#{result}... #{message}"
+  result = (expected == actual) ? "ok" : "not ok"
+  puts "#{result} - #{message}"
 end
