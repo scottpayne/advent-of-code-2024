f94e50a2875639efde97678cc890d1286985e8cf
Scotty P <scottyp@scottpayne.id.au>
2024-12-10 06:22:54 +1100

    Use enumerators for everything

--- day-9/part1.rb
+++ day-9/part1.rb
@@ -4,31 +4,28 @@ BlockRun = Data.define(:count, :contents)
 FileBlocks = Data.define(:id)
 FreeSpace = Data.define
 
-def parse(input_line)
-  input_line.chars.each_slice(2).flat_map.with_index do |pair, index|
-    [
-      BlockRun.new(count: pair[0].to_i, contents: FileBlocks.new(id: index)),
-      BlockRun.new(count: pair[1].to_i, contents: FreeSpace.new)
-    ].reject { |block_run| block_run in BlockRun(count: nil) }
+def enumerate_blocks(input_line)
+  Enumerator.new do |yielder|
+    input_line.chars.each_slice(2).with_index do |pair, index|
+      pair[0].to_i.times { yielder.yield FileBlocks.new(id: index) }
+      pair[1].to_i.times { yielder.yield FreeSpace.new }
+    end
   end
 end
 
 def checksum(blocks)
-  blocks.map.with_object({idx: 0, sum: 0}) do |block_run, acc|
-    start = acc[:idx]
-    finish = start + block_run.count
-    acc[:sum] +=
-      case block_run.contents
-      in FileBlocks(id: id)
-        (start...finish).sum { |idx| idx * id }
-      in FreeSpace
-        0
-      end
-    acc[:idx] = finish
-  end[:sum]
+  blocks.map.with_index do |block, idx|
+    case block
+    in FileBlocks(id: id)
+      id * idx
+    in FreeSpace
+      0
+    end
+  end.sum
 end
 
 input = File.read(ARGV[0]).chomp("\n")
+require "../tap_assert"
 
-# require_relative "../tap_assert"
-# require "awesome_print"
+assert 0 * 0 + 0 * 1 + 0 * 2 + 0 * 3 + 1 * 4 + 1 * 5, checksum(enumerate_blocks("222"))
+assert 0 * 0 + 0 * 1 + 1 * 2 + 1 * 3, checksum(enumerate_blocks("202"))
