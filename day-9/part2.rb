1f9604a8326f977c886c3bbcc26ca26bae5c320f
Scotty P <scottyp@scottpayne.id.au>
2024-12-10 14:50:26 +1100

    Got something working but the logic is wrong
    We don't want the highest id that fits the most space, we want the highest id that can possible fit the space

--- day-9/part2.rb
+++ day-9/part2.rb
@@ -1,6 +1,10 @@
 DiskMap = Data.define(:blocks)
 
-BlockRun = Data.define(:count, :contents)
+BlockRun = Data.define(:count, :contents) do
+  def empty?
+    count.zero?
+  end
+end
 FileBlocks = Data.define(:id) do
   def <=>(other)
     id <=> other.id
@@ -17,12 +21,47 @@ def enumerate_blocks(input_line)
   end
 end
 
-def file_id_size_map(blocks)
-  blocks
-    .select { |block| block.is_a?(FileBlocks) }
-    .tally
-    .group_by { |k, v| v }
-    .transform_values { |vs| vs.map { |v| v.first }.sort.reverse }
+def parse(input_line)
+  input_line.chars.each_slice(2).flat_map.with_index do |(file_space, free_space), index|
+    [
+      BlockRun.new(count: file_space.to_i, contents: FileBlocks[index]),
+      BlockRun.new(count: free_space.to_i, contents: FreeSpace.new)
+    ].reject(&:empty?)
+  end
+end
+
+class FileSizeMap
+  def initialize(blocks)
+    @map =
+      blocks
+        .select { |block| block in FileBlocks }
+        .tally
+        .group_by { |_, v| v }
+        .transform_values { |vs| vs.map(&:first).sort.reverse }
+    @deleted = []
+  end
+
+  def file_sizes
+    @map.keys.sort.reverse
+  end
+
+  def delete_by_size(size)
+    (@map[size] || []).shift.tap do |deleted|
+      @map.delete(size) if @map.key?(size) && @map[size].empty?
+      @deleted << deleted unless deleted.nil?
+    end
+  end
+
+  def delete(block)
+    @map.each do |k, v|
+      v.delete(block)
+      @map.delete(k) if @map[k].empty?
+    end
+  end
+
+  def deleted?(file_block)
+    @deleted.include?(file_block)
+  end
 end
 
 def checksum(blocks)
@@ -36,27 +75,40 @@ def checksum(blocks)
   end.sum
 end
 
-def defrag(blocks)
-  file_blocks = blocks.lazy.select { |block| block.is_a?(FileBlocks) }
-  file_blocks_from_end = file_blocks.reverse_each
-  file_blocks_left = file_blocks.count
+def replace_free_space(count, file_size_map, yielder)
+  available_sizes = file_size_map.file_sizes.reject { |size| size > count }
+  if available_sizes.empty?
+    count.times { yielder.yield FreeSpace.new }
+    return
+  end
 
+  next_size = available_sizes.first
+  file_block = file_size_map.delete_by_size(next_size)
+  next_size.times { yielder.yield file_block }
+
+  replace_free_space(count - next_size, file_size_map, yielder)
+end
+
+def defrag(blocks)
+  file_size_map = FileSizeMap.new(blocks)
   Enumerator.new do |yielder|
+    free_count = 0
     blocks.each do |block|
-      if file_blocks_left == 0
-        yielder.yield FreeSpace.new
-      elsif block.is_a?(FileBlocks)
-        yielder.yield block
-        file_blocks_left -= 1
+      if block.is_a?(FileBlocks)
+        replace_free_space(free_count, file_size_map, yielder)
+        free_count = 0
+        if file_size_map.deleted?(block)
+          yielder.yield FreeSpace.new
+        else
+          file_size_map.delete(block)
+          yielder.yield block
+        end
       else
-        yielder.yield file_blocks_from_end.next
-        file_blocks_left -= 1
+        free_count += 1
       end
     end
   end
 end
-
-input = File.read(ARGV[0]).chomp("\n")
 
 # puts checksum(defrag(enumerate_blocks(input)))
 
@@ -64,7 +116,17 @@ def do_tests
   require "awesome_print"
   require "../tap_assert"
 
-  assert({2 => [FileBlocks[2], FileBlocks[0]], 3 => [FileBlocks[1]]}, file_id_size_map(enumerate_blocks("20302")))
-  assert({2 => [FileBlocks[2], FileBlocks[0]], 3 => [FileBlocks[1]]}, file_id_size_map(enumerate_blocks("21312")))
+  blocks = enumerate_blocks("2342202")
+  # ap blocks.to_a
+  ap defrag(blocks).to_a
+
+  # assert({2 => [FileBlocks[2], FileBlocks[0]], 3 => [FileBlocks[1]]}, file_id_size_map(parse("20302")))
+  # assert({2 => [FileBlocks[2], FileBlocks[0]], 3 => [FileBlocks[1]]}, file_id_size_map(parse("21312")))
 end
-do_tests
+
+def main
+  input = File.read(ARGV[0]).chomp("\n")
+  puts defrag(enumerate_blocks(input)).to_a
+end
+
+main
