dab18dd35fc993dd9ff87dd51a26a807a50c320c
Scotty P <scottyp@scottpayne.id.au>
2024-12-11 16:54:14 +1100

    11.2 - taking too long as a loop, try recursion

--- day-11/part1.rb
+++ day-11/part1.rb
@@ -1,40 +1,50 @@
+require "memo_wise"
+
 Rule = Data.define(:applicable, :op)
 
-def transmogrifyer(stones, times)
-  rules = [
-    Rule.new(
-      applicable: ->(stone) { stone == 0 },
-      op: ->(stone) { 1 }
-    ),
-    Rule.new(
-      applicable: ->(stone) do
-        stone.to_s.length % 2 == 0
-      end,
-      op: ->(stone) do
-        [
-          stone.to_s[...(stone.to_s.length / 2)].to_i,
-          stone.to_s[(stone.to_s.length / 2)...].to_i
-        ]
-      end
-    ),
-    Rule.new(
-      applicable: ->(stone) { true },
-      op: ->(stone) { stone * 2024 }
-    )
-  ]
-  cache = {}
-  times.times do |n|
-    print "." if n % 10 == 0
-    stones = stones.flat_map do |stone|
-      cache[stone] ||= rules.detect { |rule| rule.applicable[stone] }.op[stone]
-    end
+class Transmogrifyer
+  prepend MemoWise
+
+  def transmogrify(stones, blinks)
+    stones.sum { |stone| transmogrify_one(stone, blinks) }
   end
-  stones
+
+  private
+
+  def transmogrify_one(stone, blinks)
+    return 1 if blinks <= 0
+    new_stones = RULES.detect { |rule| rule.applicable[stone] }.op[stone]
+    Array(new_stones).sum { |new_stone| transmogrify_one(new_stone, blinks - 1) }
+  end
+  memo_wise :transmogrify_one
+
+  RULES =
+    [
+      Rule.new(
+        applicable: ->(stone) { stone == 0 },
+        op: ->(stone) { 1 }
+      ),
+      Rule.new(
+        applicable: ->(stone) do
+          stone.to_s.length % 2 == 0
+        end,
+        op: ->(stone) do
+          [
+            stone.to_s[...(stone.to_s.length / 2)].to_i,
+            stone.to_s[(stone.to_s.length / 2)...].to_i
+          ]
+        end
+      ),
+      Rule.new(
+        applicable: ->(stone) { true },
+        op: ->(stone) { stone * 2024 }
+      )
+    ]
 end
 
 # input = "125 17"
 input = File.read(ARGV[0])
 
 stones = input.split(" ").map(&:to_i)
-result = transmogrifyer(stones, 75)
-puts result.size
+result = Transmogrifyer.new.transmogrify(stones, 75)
+puts result
