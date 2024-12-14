b04d462eff8ace5d9e226d24e6e858e166170dd6
Scotty P <scottyp@scottpayne.id.au>
2024-12-14 14:31:36 +1100

    Clean up

--- day-12/part2.rb
+++ day-12/part2.rb
@@ -34,32 +34,6 @@ class GardenMap
   end
 end
 
-def fencing_cost(position, region, map)
-  perimiter = 4 - neighbours(position).count { |n| map[n] == region }
-  area = 1
-  perimiter + area
-end
-
-# 2 regions
-# 1 of area 2
-# 1 of area 1
-# total = (a1 * p1) + (a2 * p2)
-#       = p1 + (a2 * p2) / a1
-#       = p1/p2 + a2/a1
-
-def solve(map)
-  map.sum do |position, region|
-    fencing_cost(position, region, map)
-  end
-end
-
-def contiguous?(left, right)
-  return false if left.nil? || right.nil?
-
-  right[0] == left[0] && ((right[1] - 1)..(right[1] + 1)).cover?(left[1]) ||
-    right[1] == left[1] && ((right[0] - 1)..(right[0] + 1)).cover?(left[0])
-end
-
 def calc(garden_map)
   visited = []
   garden_map.sum do |plot|
