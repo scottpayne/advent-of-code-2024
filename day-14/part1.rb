76a1f972860ca41e2408847e932ec7e6fa539e53
Scotty P <scottyp@scottpayne.id.au>
2024-12-16 18:14:54 +1100

    Day 14 - puzzle input

--- day-14/part1.rb
+++ day-14/part1.rb
@@ -13,8 +13,13 @@ require "polars"
 
 input = parse(File.read(ARGV[0]))
 data = Polars::LazyFrame.new(input)
-map_width = 11
-map_height = 7
+# map_width = 11
+# map_height = 7
+# iterations = 100
+
+map_width = 101
+map_height = 103
+iterations = 100
 
 def find_quadrant(col, boundary)
   split_value = boundary / 2
@@ -24,18 +29,37 @@ translated = data.select(
   Polars.col(:id),
   Polars.col(:sx),
   Polars.col(:sy),
-  ((Polars.col(:sx) + Polars.col(:vx) * 100) % map_width).alias("x"),
-  ((Polars.col(:sy) + Polars.col(:vy) * 100) % map_height).alias("y")
+  ((Polars.col(:sx) + Polars.col(:vx) * iterations) % map_width).alias("x"),
+  ((Polars.col(:sy) + Polars.col(:vy) * iterations) % map_height).alias("y")
 ).with_columns(
   q_x: find_quadrant(Polars.col("x"), map_width),
   q_y: find_quadrant(Polars.col("y"), map_height)
 )
 
 # translated.group_by([Polars.col("q_x"), Polars.col("q_y")]).agg(count: Polars.len).collect
-g = translated.drop_nulls.group_by([Polars.col("q_x"), Polars.col("q_y")]).agg(count: Polars.len)
+g = translated
+  .drop_nulls
+  .group_by([Polars.col("q_x"), Polars.col("q_y")])
+  .agg(count: Polars.len)
 
 def get_result(df)
   df.collect[:count].cumprod[-1]
 end
+
+def print_grid(df)
+  (0...map_width).each do |x|
+    (0...map_height).each do |y|
+      if df.filter(Polars.col("x") == x, Polars.col("y") == y).collect[:count].first > 0
+        print "#"
+      else
+        print "."
+      end
+    end
+    puts
+  end
+end
+
+# require "pry"
+# binding.pry
 
 puts get_result(g)
